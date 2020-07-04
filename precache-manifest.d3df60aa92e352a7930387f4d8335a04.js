4b72899df5ec1c3a2e4a617c96fec1bf5c260414
frostillicus <stephenlee.bus@gmail.com>
2020-07-04 00:18:35 -0400

    Updates

---
+++ precache-manifest.d3df60aa92e352a7930387f4d8335a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6974d62e6627f8f9f69aaef5e85aa11b",
+    "url": "/simple-react-storefront/index.html"
+  },
+  {
+    "revision": "9af78838e492baf9ec24",
+    "url": "/simple-react-storefront/static/css/main.7adb2ba4.chunk.css"
+  },
+  {
+    "revision": "c9ca418883bf06471b1f",
+    "url": "/simple-react-storefront/static/js/2.70e148b5.chunk.js"
+  },
+  {
+    "revision": "ebc5d087fc4c4229c0680561aa363996",
+    "url": "/simple-react-storefront/static/js/2.70e148b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af78838e492baf9ec24",
+    "url": "/simple-react-storefront/static/js/main.17eaf893.chunk.js"
+  },
+  {
+    "revision": "bb91cf9ad1f36a925258",
+    "url": "/simple-react-storefront/static/js/runtime-main.8581f18d.js"
+  }
+]);
