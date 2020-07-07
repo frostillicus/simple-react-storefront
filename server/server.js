dde2b94d86a7b37c11725cafc96b058ab0de30a2
frostillicus <stephenlee.bus@gmail.com>
2020-07-06 22:33:50 -0400

    works with node server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "/build");
+const publicPath = path.join(__dirname, "..", "build");
 
 app.use(express.static(publicPath));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(publicPath, "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 app.listen(3000, () => {
   console.log("Server is up!");
